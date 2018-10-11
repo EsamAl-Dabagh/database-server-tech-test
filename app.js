1effbf1d83beb7720fd33b83d94d4e0e32c15c21
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-10-11 13:40:45 +0100

    feat: create /get route

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/set", function(req, res) {
   res.send("This is the set route");
 });
 
+app.get("/get", function(req, res) {
+  res.send("This is the set route");
+})
+
 app.listen(4000, function() {
   console.log("The server has been started, navigate to http://localhost:4000");
 })
--- cypress/integration/set_spec.js
+++ cypress/integration/set_spec.js
@@ -6,5 +6,15 @@ describe("/set", function() {
       expect(server.status).to.eq(200);
     });
     
-  })
-})
+  });
+});
+
+describe("/get", function() {
+  it("returns a 200 status", function() {
+    cy.visit("http://localhost:4000/get");
+
+    cy.server().should(function(server) {
+      expect(server.status).to.eq(200);
+    });
+  });
+});
